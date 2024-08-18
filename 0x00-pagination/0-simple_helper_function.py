f4f29c7016369c3a326621c2f24eb83d295064af
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-18 16:26:01 +0300

    0. Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,6 @@
 """0-simple_helper_function.py"""
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> set:
     """index_range function"""
     return ((page * page_size) - page_size, page * page_size)
