feda4708e309c1efd0f8213008320fe66feb812b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-21 03:11:20 +0300

    4. MRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -7,27 +7,41 @@ class LRUCache(BaseCaching):
     """LRUCache Class"""
 
     def __init__(self):
-        self.last_used = None
         super().__init__()
+        self.last_used = []
+
+    def get_lru_item(self, usage_list):
+        """get_lru_item"""
+        seen = set()
+        lru_item = None
+
+        # Traverse the list in reverse order
+        for item in reversed(usage_list):
+            if item not in seen:
+                seen.add(item)
+                lru_item = item
+
+        return lru_item
 
     def put(self, key, item):
         """put function"""
         if key is None or item is None:
             return
         all_keys = list(self.cache_data.keys())
-
         if len(all_keys) >= BaseCaching.MAX_ITEMS and key not in all_keys:
+            to_discard = self.get_lru_item(self.last_used)
 
-            print("DISCARD: {}".format(self.last_used))
-            del self.cache_data[self.last_used]
+            print("DISCARD: {}".format(to_discard))
+            del self.cache_data[to_discard]
+            self.last_used = [i for i in self.last_used if i != to_discard]
 
-        self.last_used = key
+        self.last_used.append(key)
         self.cache_data[key] = item
 
     def get(self, key):
         """get function"""
         if key is None or key not in self.cache_data.keys():
             return None
-        self.last_used = key
+        self.last_used.append(key)
 
         return self.cache_data[key]
