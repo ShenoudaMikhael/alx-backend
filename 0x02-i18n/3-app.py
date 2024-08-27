1d68ab9a717fa88390f89255003cf8926193994e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-08-28 00:46:38 +0300

    3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,9 +26,7 @@ def get_locale():
 @app.route("/")
 def hello_world():
     """hello_world function"""
-    return render_template(
-        "3-index.html", title=_("home_title"), header=_("home_header")
-    )
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
