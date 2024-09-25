92a9fcea741215559d43c7df0b7b8f67fd931387
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-25 14:45:03 +0300

    2. Node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,11 +1,25 @@
 #!/usr/bin/node
 import { createClient } from 'redis';
 
-const client =  createClient()
+const client = createClient()
 client.on('error', err => console.log('Redis Client Error', err))
-  .connect();
+    .connect();
 
 
 client.on('connect', () => {
-  console.log('Redis client connected to the server');
+    console.log('Redis client connected to the server');
 });
+
+function setNewSchool(schoolName, value) {
+    client.SET(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+    client.GET(schoolName, (err, value) => {
+        console.log(value);
+    });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
